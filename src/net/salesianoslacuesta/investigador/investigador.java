88a96bcc05a7b69e81e2769e6772d47a4047ca4e
Samuel Afonso <usr0852@salesianos-lacuesta.net>
2026-05-28 12:55:51 +0100

    Añadí los getters y setters de la clase abstracta

--- src/net/salesianoslacuesta/investigador/investigador.java
+++ src/net/salesianoslacuesta/investigador/investigador.java
@@ -9,8 +9,35 @@ public abstract class Investigador {
         this.nombre = nombre;
         this.especialidad = especialidad;
         this.sueldo = sueldo;
-        
+
     }
 
+    // Getters
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getEspecialidad() {
+        return especialidad;
+    }
+
+    public double getSueldo() {
+        return sueldo;
+    }
+
+    // Setters
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setEspecialidad(String especialidad) {
+        this.especialidad = especialidad;
+    }
+
+    public void setSueldo(double sueldo) {
+        this.sueldo = sueldo;
+    }
 
 }
