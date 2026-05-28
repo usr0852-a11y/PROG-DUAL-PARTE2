a893b5e6505851b83763a33de6c6a25ff6d12bf0
Samuel Afonso <usr0852@salesianos-lacuesta.net>
2026-05-28 12:52:57 +0100

    Añadí la clase abstracta investigador con sus atributo y su constructor

---
+++ src/net/salesianoslacuesta/investigador/investigador.java
@@ -0,0 +1,16 @@
+package net.salesianoslacuesta.investigador;
+public abstract class Investigador {
+
+    protected String nombre;
+    protected String especialidad;
+    protected double sueldo;
+
+    public Investigador(String nombre, String especialidad, double sueldo) {
+        this.nombre = nombre;
+        this.especialidad = especialidad;
+        this.sueldo = sueldo;
+        
+    }
+
+
+}
