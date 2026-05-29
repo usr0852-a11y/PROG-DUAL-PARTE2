7ee3b836357b179b0633ef3f82cd8702d68ff3f1
Samuel Afonso <usr0852@salesianos-lacuesta.net>
2026-05-29 12:00:44 +0100

    Añadí los métodos de la clase abstracta con los requisítos que me pedía el ejercicio

--- src/net/salesianoslacuesta/matematico/Matematico.java
+++ src/net/salesianoslacuesta/matematico/Matematico.java
@@ -22,4 +22,49 @@ public class Matematico extends Investigador {
     public void setResultadosActuales(int[][] matrizInicial) {
         this.resultadosActuales = matrizInicial;
     }
+
+    public void trabajar(int numero) {
+        System.out.println("Multriplanco la matriz por: " + numero + "...");
+
+        for (int i = 0; i < 3; i++) {
+            for(int j = 0; j < 3; j++) {
+                resultadosActuales[i][j] *=numero;
+            }
+        }
+    }
+
+    public void trabajar(int[][] otraMatriz) {
+        System.out.println("Sumando matrices...");
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                resultadosActuales[i][j] += otraMatriz[i][j];
+            }
+        }
+    }
+
+    @Override
+
+    public String estadoSueldo() {
+        if (sueldo < 1800) {
+            return "Sueldo insuficiente.Solicita aumento";
+        }
+        return " Sueldo correcto!!";
+    }
+
+    @Override
+
+    public void trabajar() {
+        System.out.println("Para que el matemático trabaje necesita parámetros (int o matriz)");
+    }
+    
+    public void mostrarMatriz() {
+        System.out.println("Resultados actuales: ");
+
+        for (int i = 0 ; i < resultadosActuales.length; i++) {
+            for (int j = 0; resultadosActuales[i].length; j++) {
+                System.out.println(resultadosActuales[i][j] + " ");
+            }
+        }
+    }
 }
